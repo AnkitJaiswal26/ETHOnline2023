ba408de53752e421ecd73caccaf7de425fa5a48c
Gauri Shewale <gvshewale_b19@ce.vjti.ac.in>
2023-10-21 18:11:29 +0530

    Added Marketplace and Dashboard UI

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "react-tabs/style/react-tabs.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { LoanContextProvider } from "./context/LoanContext";
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import CustomTable from "../../components/CustomTable";
+import { nfts } from "../../consts";
 
 const Dashboard = () => {
   return (
@@ -10,7 +13,28 @@ const Dashboard = () => {
 
         <div className="flex-col">
           <Banner name="Dashboard" />
-          <div className="mx-auto py-10 px-10">Hi from Dash!</div>
+          <div className="mx-auto py-10 px-10">
+            <Tabs>
+              <TabList>
+                <Tab>My Borrowed Offers</Tab>
+                <Tab>My Lended Offers</Tab>
+              </TabList>
+              <TabPanel>
+                <CustomTable
+                  data={nfts}
+                  skipCols={["id"]}
+                  convertTime={["startDate", "endDate"]}
+                />
+              </TabPanel>
+              <TabPanel>
+                <CustomTable
+                  data={nfts}
+                  skipCols={["id"]}
+                  convertTime={["startDate", "endDate"]}
+                />
+              </TabPanel>
+            </Tabs>
+          </div>
         </div>
       </div>
     </>
