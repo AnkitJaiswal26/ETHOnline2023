9f92fb19c76f074d30550a9a7d4712c0914626cb
Ankit Bipin Bihari Jaiswal <jankitbb@gmail.com>
2023-10-22 15:29:36 +0530

    Update constants.js

--- src/context/constants.js
+++ src/context/constants.js
@@ -1,18 +1,18 @@
-// import loanContract from "../artifacts/contracts/LoanContract.sol/LoanContract.json";
+import loanContract from "../artifacts/contracts/LoanContract.sol/LoanContract.json";
 
-// export const LoanContractAddress = "0x108A19d630bd6a11EB259Ca9DB972B608fEB1837";
-// export const LoanContractABI = loanContract.abi;
+export const LoanContractAddress = "0xC54e419Ca67DbC02552eF1c9a401c57f2302F0e8";
+export const LoanContractABI = loanContract.abi;
 
-// export const ChainId = {
-//     MAINNET: 1,
-//     GOERLI: 5,
-//     POLYGON_MUMBAI: 80001,
-//     POLYGON_MAINNET: 137,
-// };
+export const ChainId = {
+    MAINNET: 1,
+    GOERLI: 5,
+    POLYGON_MUMBAI: 80001,
+    POLYGON_MAINNET: 137,
+};
 
-// export let activeChainId = ChainId.POLYGON_MUMBAI;
-// export const supportedChains = [
-//     ChainId.GOERLI,
-//     ChainId.POLYGON_MAINNET,
-//     ChainId.POLYGON_MUMBAI,
-// ];
+export let activeChainId = ChainId.POLYGON_MUMBAI;
+export const supportedChains = [
+    ChainId.GOERLI,
+    ChainId.POLYGON_MAINNET,
+    ChainId.POLYGON_MUMBAI,
+];
